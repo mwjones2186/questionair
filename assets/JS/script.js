563b6472f58318e7934c4930909c2cfdca2424c5
Jonsey <mwjones2186@gmail.com>
2022-05-25 22:11:00 -0700

    getting comments in and psudocode in for next step. Still more psuedocode to go but Im tired and need to sleep.

---
+++ assets/JS/script.js
@@ -0,0 +1,20 @@
+//omg so much to do!!
+
+//page layout
+//containers
+
+
+//first things first-HEADER HEADER HEADER!!!!!
+//work on a high score link
+//work on a coundown timer
+
+
+
+//BODY BODY
+//need a first page. are you ready to start the questionaire?
+//start quiz button
+
+//10 questions
+//4 multiple choice answers
+//display a "correct" or "wrong" display at bottom when an answer is choosen 
+//
