d5e7cfe6e71befecdb683e1e42202eccc8951547
Jonsey <mwjones2186@gmail.com>
2022-05-26 21:21:20 -0700

    added some HTML items. working on JS. Have my timer function. Needs to be modified a bit but overall functioning

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -6,7 +6,38 @@
 
 //first things first-HEADER HEADER HEADER!!!!!
 //work on a high score link
+var welcome = 
+var userName = 
+
+function welcome() {
+    prompt = ("Welcome to Questionaire! Please enter your initials")
+    
+}
+
+
+
+
+
 //work on a coundown timer
+var countDown = document.querySelector("#timer");
+var timeEl = 10;
+countDown.textContent= timeEl;
+var intervalID;
+
+intervalID = setInterval(function(){
+    timeEl--;
+    countDown.textContent= timeEl;
+    if (timeEl <= 0) {
+        
+        stop()
+  }
+}, 1000)
+
+function stop(){
+  clearInterval(intervalID);
+  alert ("The timer has reached 0!");
+  
+}
 
 
 
@@ -17,4 +48,10 @@
 //10 questions
 //4 multiple choice answers
 //display a "correct" or "wrong" display at bottom when an answer is choosen 
+
+
+
+// some of the codes i need to create 
+//while loops
+//for loops
 //
